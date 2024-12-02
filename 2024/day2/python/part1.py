0d40e95049c8f0ef2f97ea6afb5b43d78863ad71
Charles de Sainte Maresville <charles.dsm@icloud.com>
2024-12-02 19:59:38 +0100

    day2

--- 2024/day2/python/part1.py
+++ 2024/day2/python/part1.py
@@ -9,5 +9,38 @@ RELATIVE_PATH = "../inputs/input.txt"
 def solution():
   """Solution"""
 
+  safe_reports_count = 0
+
+  with open(RELATIVE_PATH, 'r', encoding='utf-8') as f:
+    for line in f.readlines() :
+      levels = list(map(int, line.split(' ')))
+
+      safe_reports_count += int(is_report_safe(levels))
+
+  return safe_reports_count
+
+def is_report_safe(levels: list):
+  """Tells whether a report is safe or note according to the rules"""
+
+  is_ascending = True
+  is_descending = True
+
+  report_size = len(levels)
+
+  if report_size > 1 :
+    previous_level = levels[0]
+    for index in range(1, report_size):
+      current_level = levels[index]
+      if (current_level >= previous_level or previous_level - current_level > 3) :
+        is_descending = False
+      if (current_level <= previous_level or current_level - previous_level > 3) :
+        is_ascending = False
+
+      previous_level = current_level
+
+  return is_ascending or is_descending
+
+
+
 if __name__ == "__main__":
   print(solution())
--- 2024/day2/python/part2.py
+++ 2024/day2/python/part2.py
@@ -9,5 +9,63 @@ RELATIVE_PATH = "../inputs/input.txt"
 def solution():
   """Solution"""
 
+  safe_reports_count = 0
+
+  with open(RELATIVE_PATH, 'r', encoding='utf-8') as f:
+    for line in f.readlines() :
+      levels = list(map(int, line.split(' ')))
+
+      safe_reports_count += int(is_report_safe_with_one_error(levels))
+
+  return safe_reports_count
+
+def is_report_safe_with_one_error(levels: list):
+  """Tells whether a report is safe with a one error tolerence"""
+
+  report_size = len(levels)
+
+  for index in range(report_size) :
+    levels_with_one_error = levels[:index] + levels[index + 1:]
+
+    if is_report_safe(levels_with_one_error) :
+      return True
+
+  return False
+
+def is_report_safe(levels: list):
+  """Tells whether a report is safe or note according to the rules"""
+
+  is_ascending, is_descending = True, True
+  report_size = len(levels)
+
+  if report_size > 1 :
+    previous_level = levels[0]
+    for index in range(1, report_size):
+      current_level = levels[index]
+
+      if check_asc(current_level, previous_level) :
+        is_ascending = False
+
+      if check_desc(current_level, previous_level) :
+        is_descending = False
+
+      if not is_ascending and not is_descending:
+        return False
+
+      previous_level = current_level
+
+  return is_ascending or is_descending
+
+def check_asc(level_a, level_b):
+  """check is level_a and level_b follows the ascending rule"""
+
+  return level_a <= level_b or level_a - level_b > 3
+
+def check_desc(level_a, level_b):
+  """check is level_a and level_b follows the descending rule"""
+
+  return level_a >= level_b or level_b - level_a > 3
+
+
 if __name__ == "__main__":
   print(solution())
