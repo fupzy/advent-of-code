52da3261e6357186266bfd0751fb04c18d93182b
Charles de Sainte Maresville <charles.dsm@icloud.com>
2024-12-04 21:24:25 +0100

    day4

--- 2024/day4/python/part1.py
+++ 2024/day4/python/part1.py
@@ -4,10 +4,67 @@
 Advent of Code Day 4 Part 1.
 """
 
+from collections import defaultdict
+
 RELATIVE_PATH = "../inputs/input.txt"
+TEST = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
 
 def solution():
   """Solution"""
+
+  letter_matrix = [[]]
+
+  with open(RELATIVE_PATH, 'r', encoding='utf-8') as f:
+    for line in f.readlines() :
+      matrix_line = []
+      for letter in line:
+        if letter != '\n':
+          matrix_line.append(letter)
+      letter_matrix.append(matrix_line)
+
+  columns = groups(letter_matrix, lambda x, y: x)
+  columns = [''.join(col) for col in columns]
+
+  rows = groups(letter_matrix, lambda x, y: y)
+  rows = [''.join(row) for row in rows]
+
+  diagonals = groups(letter_matrix, lambda x, y: x + y)
+  diagonals = [''.join(diag) for diag in diagonals]
+
+  anti_diagonals = groups(letter_matrix, lambda x, y: x - y)
+  anti_diagonals = [''.join(anti_diag) for anti_diag in anti_diagonals]
+
+  return find_all_xmas(columns) + find_all_xmas(rows) + find_all_xmas(diagonals) + find_all_xmas(anti_diagonals)
+
+def find_all_xmas(lists):
+  """Find XMAS and SAMX in all lists of lists"""
+
+  xmas_sum = 0
+
+  for list in lists:
+    xmas_sum += find_xmas(list)
+
+  return xmas_sum
+
+def find_xmas(list: list):
+  """Find XMAS and SAMX in the list"""
+
+  return list.count('XMAS') + list.count('SAMX')
+
+def groups(data, func):
+  """Grouping"""
+
+  grouping = defaultdict(list)
+  for y in range(len(data)):
+    for x in range(len(data[y])):
+      grouping[func(x, y)].append(data[y][x])
+
+  return list(map(grouping.get, sorted(grouping)))
+
 
 if __name__ == "__main__":
   print(solution())
--- 2024/day4/python/part2.py
+++ 2024/day4/python/part2.py
@@ -5,9 +5,43 @@ Advent of Code Day 4 Part 2.
 """
 
 RELATIVE_PATH = "../inputs/input.txt"
+RELATIVE_PATH_TEST = "../inputs/short.txt"
 
 def solution():
   """Solution"""
+
+  letter_matrix = []
+
+  with open(RELATIVE_PATH, 'r', encoding='utf-8') as f:
+    for line in f.readlines() :
+      matrix_line = []
+      for letter in line:
+        if letter != '\n':
+          matrix_line.append(letter)
+      letter_matrix.append(matrix_line)
+
+  xmas_sum = 0
+
+  lines = len(letter_matrix)
+  columns = len(letter_matrix[0])
+
+  for i in range(lines - 2):
+    for j in range(columns - 2):
+        sub_matrix = [row[j:j+3] for row in letter_matrix[i:i+3]]
+
+        xmas_sum += int(find_xmas(sub_matrix))
+
+  return xmas_sum
+
+def find_xmas(matrix):
+  """Find X-MAS in 3*3 matrix"""
+
+  target = ('MAS', 'SAM')
+
+  diag = matrix[0][0] + matrix[1][1] + matrix[2][2]
+  anti_diag = matrix[0][2] + matrix[1][1] + matrix[2][0]
+
+  return diag in target and anti_diag in target
 
 if __name__ == "__main__":
   print(solution())
